b6047794d632dec7eb3a04f3227d5604f9686573
Snehanshu Phukon <snehanshuphukan@gmail.com>
2020-02-26 10:58:50 +0530

    ceaser cipher implemented

---
+++ ceaser.js
@@ -0,0 +1,23 @@
+module.exports = ceaser;
+
+/**
+ * Get the ceaser cipher of any text
+ * @param {String} text The string to encrypt
+ * @param {Number} key The ceaser key. Default = 5
+ * @returns {String}
+ */
+function ceaser (text = '', key = 5) {
+    let ceaserText = text.split('').map(t => getCeaser(t, key));
+    return ceaserText.join('');
+}
+
+function getCeaser(t, key = 5) {
+    let charSet = 'abcdedghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'.split('');
+    if (!charSet.includes(t)) return t;
+    let newKey = charSet.indexOf(t) + key;
+    if (newKey >= charSet.length) {
+        newKey = (newKey % charSet.length)
+    }
+    return charSet[newKey];
+}
+
