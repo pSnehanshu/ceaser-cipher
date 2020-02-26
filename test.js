4024ba1d392930b50f5394664cefeea61d25cd90
Snehanshu Phukon <snehanshuphukan@gmail.com>
2020-02-26 11:19:56 +0530

    test

--- test.js
+++ test.js
@@ -5,5 +5,4 @@ let inputText = readline.question('Enter a text: ');
 let key = readline.questionInt('Enter a key [INT] (default: 5): ', {
     defaultInput: 5,
 });
-
 console.log('Ceaser cipher:', ceaser(inputText, key));
